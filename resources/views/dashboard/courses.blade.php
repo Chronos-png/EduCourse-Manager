d54ccf0cc4294e81d9aa36a7b54ea3bb39e15878
Chronos-png <ychronos13@gmail.com>
2025-01-28 15:09:52 +0700

    Improve UI dashboard student for courses

--- resources/views/dashboard/courses.blade.php
+++ resources/views/dashboard/courses.blade.php
@@ -65,20 +65,39 @@
                             </p>
                             <div class="flex-grow h-full">
                                 <div class="flex-grow h-full flex justify-between items-center mt-auto space-x-4">
-                                    <button
-                                        onclick="enrollnow('{{ $course->id_kursus }}', '{{ $course->nama_kursus }}', {{ $course->harga }})"
-                                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        Enroll Now
-                                    </button>
-                                    <div class="flex items-center space-x-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                        </svg>
-                                        <span class="text-sm font-medium">IDR
-                                            {{ number_format($course->harga, 0, ',', '.') }}</span>
-                                    </div>
+                                    @if ($course->status_kursus == 'active')
+                                        <button
+                                            onclick="enrollnow('{{ $course->id_kursus }}', '{{ $course->nama_kursus }}', {{ $course->harga }})"
+                                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            Enroll Now
+                                        </button>
+
+                                        <div class="flex items-center space-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                            </svg>
+                                            <span class="text-sm font-medium">IDR
+                                                {{ number_format($course->harga, 0, ',', '.') }}</span>
+                                        </div>
+                                    @else
+                                        <button disabled
+                                            onclick="enrollnow('{{ $course->id_kursus }}', '{{ $course->nama_kursus }}', {{ $course->harga }})"
+                                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none">
+                                            Course Closed
+                                        </button>
+
+                                        <div class="flex items-center space-x-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                            </svg>
+                                            <span class="text-sm font-medium line-through">IDR
+                                                {{ number_format($course->harga, 0, ',', '.') }}</span>
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
 
