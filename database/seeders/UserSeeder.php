730f7f9099e30890e2712257b59625b880173493
Chronos-png <ychronos13@gmail.com>
2025-01-28 09:45:36 +0700

    add user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,21 +16,105 @@ class UserSeeder extends Seeder
     {
         // Create a regular user
         User::create([
-            'name' => 'John Doe',
-            'username' => 'john_doe',
-            'email' => 'john.doe@example.com',
+            'name' => 'Adit Kurniawan',
+            'username' => 'ditt_adit',
+            'email' => 'adit@example.com',
             'email_verified_at' => now(), // Set email_verified_at for a verified user
-            'password' => Hash::make('password123'), // Hash the password
+            'password' => Hash::make('adit123'), // Hash the password
+            'usertype' => 'user', // Default 'user'
+        ]);
+        User::create([
+            'name' => 'Ilham Maulana',
+            'username' => 'ilham_maulana',
+            'email' => 'ilhamham@example.com',
+            'email_verified_at' => now(), // Set email_verified_at for a verified user
+            'password' => Hash::make('namakuilham123'), // Hash the password
+            'usertype' => 'user', // Default 'user'
+        ]);
+        User::create([
+            'name' => 'Toni Sukmawan',
+            'username' => 'sukmaton97',
+            'email' => 'sukma@example.com',
+            'email_verified_at' => now(), // Set email_verified_at for a verified user
+            'password' => Hash::make('hillenburg37'), // Hash the password
+            'usertype' => 'user', // Default 'user'
+        ]);
+        User::create([
+            'name' => 'Siti Rahmawati',
+            'username' => 'rahma_siti',
+            'email' => 'rahmawatisiti@example.com',
+            'email_verified_at' => now(), // Set email_verified_at for a verified user
+            'password' => Hash::make('sitirahma88'), // Hash the password
+            'usertype' => 'user', // Default 'user'
+        ]);
+        User::create([
+            'name' => 'Andi Saputra',
+            'username' => 'andi_saputra',
+            'email' => 'andi@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('andi123'),
+            'usertype' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Budi Prasetyo',
+            'username' => 'budi_prasetyo',
+            'email' => 'budi@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('budiprasetyo456'),
+            'usertype' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Citra Dewi',
+            'username' => 'citra_dewi',
+            'email' => 'citra@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('citra2025'),
+            'usertype' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Dewi Lestari',
+            'username' => 'dewi_lestari',
+            'email' => 'dewi@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('dewiL123'),
+            'usertype' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Fajar Setiawan',
+            'username' => 'fajar_setiawan',
+            'email' => 'fajar@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('fajar456'),
+            'usertype' => 'user',
+        ]);
+        User::create([
+            'name' => 'User EduCourse',
+            'username' => 'user',
+            'email' => 'user@example.com',
+            'email_verified_at' => now(), // Set email_verified_at for a verified user
+            'password' => Hash::make('user123'), // Hash the password
             'usertype' => 'user', // Default 'user'
         ]);
 
         // Create an admin user
         User::create([
-            'name' => 'Admin User',
-            'username' => 'admin_user',
-            'email' => 'admin@example.com',
+            'name' => 'Rosalinda Putri',
+            'username' => 'linrosa97',
+            'email' => 'linrosa_rosalinda@example.com',
             'email_verified_at' => now(), // Set email_verified_at for a verified admin user
             'password' => Hash::make('adminpassword'), // Hash the password
+            'usertype' => 'admin', // Admin user
+        ]);
+        User::create([
+            'name' => 'Admin EduCourse',
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(), // Set email_verified_at for a verified admin user
+            'password' => Hash::make('admin123'), // Hash the password
             'usertype' => 'admin', // Admin user
         ]);
 
